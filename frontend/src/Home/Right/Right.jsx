fb6c035f5a0bd38f3cc7edb499f6affa65b3302d
aadi1232 <aadipatel1911@gmail.com>
2024-11-02 17:49:21 +0530

    check

--- frontend/src/Home/Right/Right.jsx
+++ frontend/src/Home/Right/Right.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Chatuser from "./chatuser";
 function Right() {
-  return <div className="  w-[70%] bg-slate-950 text-white ">Right</div>;
+  return <div className="  w-[70%] bg-slate-950 text-white ">
+  Right
+
+  <Chatuser/>
+  </div>;
 }
 
 export default Right;
---
+++ frontend/src/Home/Right/chatuser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function chatuser() {
+  return (
+    <div>
+    chatuser
+    </div>
+  )
+}
+
+export default chatuser
